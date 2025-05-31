c7eb62179f163993b76ea8e175dedcc1807905ac
CaonVini <83681064+CaonVini@users.noreply.github.com>
2025-05-31 13:32:12 -0300

    feat: Adiciona Vercel Analytics e Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Webhookly - Domine Seus Webhooks com Facilidade.',
@@ -15,6 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics></Analytics>
+      <SpeedInsights></SpeedInsights>
     </html>
   )
 }
