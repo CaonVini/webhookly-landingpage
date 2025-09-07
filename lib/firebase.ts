a1b4cb7b795bd5c384fa6ea606f7b6888c4db0ef
CaonVini <83681064+CaonVini@users.noreply.github.com>
2025-09-07 20:56:14 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,9 +1,9 @@
-// No seu arquivo de configuração do Firebase (ex: @/lib/firebase.js)
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Use o prefixo 'NEXT_PUBLIC_'
+
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
